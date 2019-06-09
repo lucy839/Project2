d14dc3f835106dd258ebd652ff11d528ad88d796
smason27 <masonsteven27@gmail.com>
2019-06-09 16:09:35 -0700

    changed index.handlebars to handle user login and signup, added passport.js to authenticate user once profile is created, added user database to store name, email, and password. still need to write the script to add users to the database and to get passport working correctly.

---
+++ models/user.js
@@ -0,0 +1,29 @@
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+
+    });
+
+    // Links User model to Upload Model
+    User.associate = function(models) {
+        User.hasMany(models.Upload)
+    };
+    
+    return User;
+   
+}
