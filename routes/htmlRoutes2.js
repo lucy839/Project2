54b5936bb32b839474d5478b2df3843eb6a355fc
Youngju Lee <lucylee839@hotmail.com>
2019-06-10 19:06:18 -0700

    basic data upload completed except for image

---
+++ routes/htmlRoutes2.js
@@ -0,0 +1,39 @@
+var db = require("../models");
+
+module.exports = function (app) {
+  // Load index page
+  app.get("/", function (req, res) {
+    // db.Example.findAll({}).then(function(dbExamples) {
+    //   res.render("index", {
+    //     msg: "Welcome!",
+    //     examples: dbExamples
+    //   });
+    // });
+    db.Upload.findAll({
+      // where: query
+      // edit needed work with Steven
+      // , include: [db.User]
+    }).then(function (dbUpload) {
+      // res.json(dbUpload);
+      res.render("index", { Upload: dbUpload });
+    });
+  });
+
+  // Load example page and pass in an example by id
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+
+  // uploading page
+    app.get('/upload', function(req, res) {
+        res.render("upload");
+    });
+  // Render 404 page for any unmatched routes
+  app.get("*", function (req, res) {
+    res.render("404");
+  });
+};
--- routes/upload-api-routes.js
+++ routes/upload-api-routes.js
@@ -2,31 +2,16 @@
 var db = require("../models");
 
 module.exports = function (app) {
-    // list items
-    app.get("/api/upload", function (req, res) {
-        var query = {};
-        // edit needed work with Steven
-        if (req.query.user_id) {
-            query.UserId = req.query.user_id;
-        }
-        // 1. Add a join here to include all of the Authors to these posts
-        db.Upload.findAll({
-            where: query
-            // edit needed work with Steven
-            // , include: [db.User]
-        }).then(function (dbUpload) {
-            res.json(dbUpload);
-        });
-    });
-
     // upload product
     app.post("/api/upload", function (req, res) {
         db.Upload.create({
             product_name: req.body.product_name,
             description: req.body.description,
             file_path: req.body.file_path
-        }).then(function (upload) {
-            res.json(upload);
-        });
+        }).then(function (dbUpload) {
+            // res.json(dbUpload);
+            res.redirect("/");
+            // res.render("index",{Upload: dbUpload} );
+        });   
     });
 };
