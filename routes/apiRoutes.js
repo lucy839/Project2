3e0de7e52d65a4e1a19d0bec24f1eb069803be4c
Casey <caseyclinga@gmail.com>
2019-06-14 19:16:52 -0700

    Created trade-api-routes using sequelize and deleted ORM

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,88 +1,35 @@
-//Use express 
-var express = require("express")
-var router = express.Router();
+// Routes
 
-//Import the model to use database functions for interaction with mysql
-var db = require("../models")
 
-//ROUTES
-//Root route will get all items in the trades table
-router.get("/", function (req, res) {
-  db.trade.available(function (data) {
-      var tradeObj = {
-          //"trades" keyword here will be used to pass data to index.handlebars
-          items: data
-      }
-      console.log(tradeObj)
-      //Send (or render) the burgObj to index
-      res.render("index", tradeObj)
-  })
-})
+var db = require("../models");
 
-//Route will get all items available from this specific user in the trades table
-router.get("/items/:id", function (req, res) {
-    //Assumed a column name of 'userId' to reference the user under the uploads table
-    db.trade.userItems(vals, function (data) {
-    //Save user id to vals
-    vals = req.param.id  
+module.exports = function(app) {
+  // Get all available items
+  app.get("/", function(req, res) {
+    db.Index.findAll({
+        where: { available: true } 
+    }).then(function(item) {
+      res.json(item);
+    });
+  });
 
-    var tradeObj = {
-          //"trades" keyword here will be used to pass data to index.handlebars
-          userItems: data
-      }
-      console.log(tradeObj)
-      //Send (or render) the burgObj to index
-      res.render("trades", tradeObj)
-  })
-})
-
-//This route will change requested value after user clicks the trade button
-router.put("/items/:id", function (req, res) {
-  var condition = {
-      userItem: "id = " + req.params.id,
-      //Acces the id of the requested item
-      reqItem: reqitem
+  var availability = {
+      availble: false
   }
-  
-  //Change available status for user's item
-  db.trade.update(condition, function (result) {
-      if (result.changedRows == 0) {
-          return res.status(404).end();
-      } else {
-          res.status(200).end()
-      }
-  })
-})
-
-
-//Export routes via router
-module.exports = router
-
-//Routes
-
-//Root route will get all burgers in the burgers table
-
-// var db = require("../models");
-
-// module.exports = function(app) {
-//   // Get all examples
-//   app.get("/api/examples", function(req, res) {
-//     db.Example.available().then(function(dbExamples) {
-//       res.json(dbExamples);
-//     });
-//   });
-
-//   // Create a new example
-//   app.post("/api/examples", function(req, res) {
-//     db.Example.create(req.body).then(function(dbExample) {
-//       res.json(dbExample);
-//     });
-//   });
-
-//   // Delete an example by id
-//   app.delete("/api/examples/:id", function(req, res) {
-//     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-//       res.json(dbExample);
-//     });
-//   });
-// };
+  // Update availability status
+  app.put("/api/availability/:id", function(req, res) {
+    db.Upload.update(availability, {
+        where: { id: req.params.id }
+    }.then(function(item) {
+      res.json(item);
+    })
+    })
+}
+  // Delete a lsiting by id
+  app.delete("/api/destroy/:id", function(req, res) {
+    db.Example.destroy({ where: { id: req.params.id } })
+    .then(function(item) {
+      res.json(item);
+    });
+  });
+}
